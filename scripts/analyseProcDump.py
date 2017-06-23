b23d146fcefac9e3d297c77cbb89d54664e4445d
Shashank Gangrade <ssk1328@users.noreply.github.com>
2017-06-23 15:31:28 +0530

    Packet flight reports generated

--- scripts/analyseProcDump.py
+++ scripts/analyseProcDump.py
@@ -11,7 +11,7 @@
 ## Read the files into dump tables
 ## Pop the entries out one by one in ascending order of cycle count
 ## If Sent packet type, then create new entry in Packet List and leave empty spot for receive cycle time
-## If Received packet type, then fill the empty spot in the first packet that matches the (src,dest)
+## If Received packet type, then fill the empty spot in the first packet that matches the (src,dest,loc)
 ## Repeat 2,3,4 till entries exist in the table
 ## Calculate the hop time for each packet and sum it up
 
@@ -19,21 +19,21 @@
 
 import sys
 if len(sys.argv) >= 2:
-    if sys.argv[1] == "mesh":
+    if sys.argv[1] == "mcf":
 
-        dumpFileName = "./proc_dump_"
-        reportFileName = "reportSimMeshNetworkTraffic.txt"
+        dumpFileName = "proc_mcf/proc_dump_"
+        reportFileName = "reportSim_mcf.txt"
 
-    elif sys.argv[1] == "pg":
+    elif sys.argv[1] == "xy":
 
-        dumpFileName = "./proc_dump_"
-        reportFileName = "reportSimPGNetworkTraffic.txt"
+        dumpFileName = "proc_xy/proc_dump_"
+        reportFileName = "reportSimP_xy.txt"
 
     else :
-        print "Specify argument 'mesh' or 'pg'\n "
+        print "Specify argument 'mcf' or 'xy'\n "
         exit()
 else : 
-    print "Specify argument 'mesh' or 'pg'\n "
+    print "Specify argument 'mcf' or 'xy'\n "
     exit()
 
 ################ CREATING DUMP TABLE ################
@@ -50,7 +50,7 @@ for i in range(numCores):
     
     for j in range(len(dumpLines)):
         dumpEntry = dumpLines[j].split(',')
-        
+
         entryProc = dumpEntry[0].split(":")
         entryProcVal = int(entryProc[1])
 
@@ -60,18 +60,24 @@ for i in range(numCores):
 
         entryDest = dumpEntry[3].split(":")
 
-        entryPacketType = dumpEntry[4]
-        
-        ## Structure of Dictionary : {'Cycle', 'Src', 'Dest' , 'PacketType'}
-        dumpTable[entryProcVal].append({entryCycle[0]:int(entryCycle[1]),
-                                        entrySrc[0]:int(entrySrc[1]),
-                                        entryDest[0]:int(entryDest[1]),
-                                        "PacketType":entryPacketType})
+        entryPacketLoc = dumpEntry[4].split(":")
+
+        entryPacketType = dumpEntry[5]
+
+
+        ## Structure of Dictionary : {'Cycle', 'Src', 'Dest' ,'PacketLoc', 'PacketType'}
+        dumpTable[entryProcVal].append({
+                                        entryCycle[0]   :int(entryCycle[1]),
+                                        entrySrc[0]     :int(entrySrc[1]),
+                                        entryDest[0]    :int(entryDest[1]),
+                                        "PacketLoc"     :int(entryPacketLoc[1]),
+                                        "PacketType"    :entryPacketType 
+                                        })
 
     
 #######################################################################
-#for i in range(numCores):
-#    print str(i)+" is Proc NUmber \n"
+# for i in range(numCores):
+#    print str(i)+" is Proc Number \n"
 #    print dumpTable[i]
 #    print "\n"
 #######################################################################
@@ -89,7 +95,7 @@ while ( done == 0):
         if(len(dumpTable[i]) is not 0):
             toPopProc = i
             smallestCycleCount = dumpTable[i][0]["Cycle"]
-##          print "Init toPopProc is :"+str(toPopProc)+" and cycle count is : "+str(smallestCycleCount);
+            # print "Init toPopProc is :"+str(toPopProc)+" and cycle count is : "+str(smallestCycleCount);
             break
     
     ## Find the dumpTable entry with earliest cycle count
@@ -105,13 +111,13 @@ while ( done == 0):
 
     ## Fill packetTable with toPopEntry
     if(toPopEntry["PacketType"] == "Sent"):
-        packetTable.append({"PacketID":str(toPopEntry["Src"])+":"+str(toPopEntry["Dest"]),
+        packetTable.append({"PacketID":str(toPopEntry["Src"])+":"+str(toPopEntry["Dest"])+":"+str(toPopEntry["PacketLoc"]) ,
                             "Sent Cycle":toPopEntry["Cycle"],
                             "Received Cycle":"?" })
 
     if(toPopEntry["PacketType"] == "Received"):
         for j in range(len(packetTable)):
-            if( packetTable[j]["PacketID"] == (str(toPopEntry["Src"])+":"+str(toPopEntry["Dest"]))):
+            if( packetTable[j]["PacketID"] == ( str(toPopEntry["Src"])+":"+str(toPopEntry["Dest"])+":"+str(toPopEntry["PacketLoc"])  )):
                 packetTable[j]["Received Cycle"] = toPopEntry["Cycle"]
                 break
     
@@ -128,6 +134,11 @@ while ( done == 0):
         done = 1
 
 print "Number of Packets is : %d"%len(packetTable)
+
+#######################################################################
+# for i in packetTable:
+#    print i
+#######################################################################
 
 #######################################################################
 
@@ -149,6 +160,9 @@ reportFile.write("\n\n###########################################\n")
 reportFile.write("Total Cycles in Flight for all Packets - "+str(totalCycles)+"\n");
 reportFile.write("###########################################\n")
     
+reportFile.write("\n\n###########################################\n")
+reportFile.write("Average number of Cycles in Flight for all Packets - "+str(float(totalCycles/len(packetTable)))+"\n");
+reportFile.write("###########################################\n")
     
 
 
